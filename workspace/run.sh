cf3d78b888ae606e82d0402be51243d77ac6b803
Nguyễn Phúc Vinh <136446610+vinhngph@users.noreply.github.com>
2026-05-24 17:21:19 +0700

    feat: ssh and gpg sync

--- workspace/run.sh
+++ workspace/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 podman run -it \
-    --userns=keep-id \
-    --mount type=bind,src=$XDG_RUNTIME_DIR,dst=/run/user/1000,rw,rslave \
-    --mount type=bind,src=$HOME/.gitconfig,dst=/home/friday/.gitconfig,readonly,rslave \
-    --mount type=bind,src=$HOME/.gnupg,dst=/home/friday/.gnupg,readonly,rslave \
-    --mount type=bind,src=$HOME/workspace,dst=/home/friday/workspace,rw,rslave \
+    -p 2222:22 \
+    -v workspace-ssh:/home/friday/.ssh \
+    -v workspace-gpg:/home/friday/.gnupg \
+    -v workspace-workspace:/home/friday/workspace \
     --device nvidia.com/gpu=all \
+    --network workspace \
     --name workspace \
     --hostname workspace \
     vinhnp/workspace
